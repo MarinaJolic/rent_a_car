ae580883eb6ab7cb9881460c5d4e3e18660b37a3
MarinaJolic <marina.jolic@fpmoz.sum.ba>
2021-04-30 19:47:12 +0200

    uređene datoteke

--- db.php
+++ db.php
@@ -7,8 +7,8 @@ define("BAZA","rent_a_car");
 
 $konekcija = mysqli_connect(RAČUNALO,KORISNIK, LOZINKA,BAZA);
 
-if (!$konekcija00) {
-    dir("NIsmo se spojili na bazu iz nekog razloga, možda znate o čemu se radi, evo greška:".mysqli_connect_error())
+if (!$konekcija) {
+    die("NIsmo se spojili na bazu iz nekog razloga, možda znate o čemu se radi, evo greška:".mysqli_connect_error());
 }
 
 
